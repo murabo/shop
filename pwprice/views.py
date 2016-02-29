a674359e006388318bb62019a5d28e6bd6eecd66
Shinya Murakami <murakami.shinya@gmail.com>
2016-02-29 23:56:35 +0900

    開業対策

--- pwprice/views.py
+++ pwprice/views.py
@@ -121,9 +121,10 @@ def _check_ng(kwd, ng_list):
 
     if ng_list and not ng_list[0]:
         return False
+
     for ng in ng_list:
         if (mojimoji.zen_to_han(kwd.decode('utf-8'), kana=False).encode('utf-8').lower().find(ng) > 0
-            or mojimoji.zen_to_han(ng.decode('utf-8'), kana=False).lower() in mojimoji.zen_to_han(kwd.lower().decode('utf-8'), kana=False)):
+            or mojimoji.zen_to_han(ng.decode('utf-8'), kana=False).lower() in mojimoji.zen_to_han(kwd.lower().decode('utf-8'), kana=False)) and ng:
             return True
 
 def _get_redis(key):
