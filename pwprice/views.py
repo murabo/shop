9e8e8c06e3908e0862b5ee5ef5701cc7bb63e5ae
Shinya Murakami <murakami.shinya@gmail.com>
2016-02-29 21:45:39 +0900

    条件修正

--- pwprice/views.py
+++ pwprice/views.py
@@ -119,7 +119,7 @@ def _check_crawler_ua(request):
 
 def _check_ng(kwd, ng_list):
     for ng in ng_list:
-        if (mojimoji.zen_to_han(kwd.decode('utf-8'), kana=False).encode('utf-8').lower().find(ng)
+        if (mojimoji.zen_to_han(kwd.decode('utf-8'), kana=False).encode('utf-8').lower().find(ng) > 0
             or mojimoji.zen_to_han(ng.decode('utf-8'), kana=False).lower() in mojimoji.zen_to_han(kwd.lower().decode('utf-8'), kana=False)):
             return True
 
